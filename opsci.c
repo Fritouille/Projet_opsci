a84ad18df8a2b395136d656e2c2cb8cadcd97281
Luc <128496211+Fritouille@users.noreply.github.com>
2024-03-09 16:40:25 +0100

    Add files via upload

---
+++ opsci.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {    
+        printf("Usage : ./opsci [up / down]\n");
+        printf("Pour [up / down] le docker compose\n");
+        return 1;
+    }
+    else{
+        if(strcmp(argv[1],"up") == 0){
+            system("docker compose up");
+            return 0;
+        }
+
+        if(strcmp(argv[1],"down") == 0){
+            system("docker compose down");
+            return 0;
+        }
+        printf("Usage : ./opsci [up / down]\n");
+        printf("Pour [up / down] le docker compose\n");
+    }
+    return 1;
+}
